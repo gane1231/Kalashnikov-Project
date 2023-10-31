10811f562d40fa86c9e9bbe2c5716f78bbb623d5
gane1231 <118179020+gane1231@users.noreply.github.com>
2023-10-31 17:41:14 +0400

    ffff

--- project/js/script.js
+++ project/js/script.js
@@ -7,10 +7,65 @@ new Swiper('.image-slider',{
 	//Прогрессбар
 	pagination: {
 		el: '.swiper-pagination',
+		// Буллеты
 		clickable: true,
+		// Динамические буллеты
+		dynamicBullets: true,
 	},
 	//Скроллбар
 	scrollbar: {
 		el: '.swiper-scrollbar',
 	},
+	simulateTouch: true,
+
+	grabCursor: true,
+	// Управление клавиатурой
+	keyboard: {
+		// Вкл/Выкл
+		enabled: true,
+		// Вкл/Выкл
+		// pgUp/pgDn
+		pageUpDown: false,
+	},
+	// Управление колесом мыши
+	mousewheel: {
+		// Чувствительность
+		sensivity: 1,
+		// Класс объекта на котором будет срабатывать прокрутка мышкой
+		//eventsTarget: ".image-slider"
+	},
+	// Автовысота
+	autoHeight: true,
+	// Кол-во слайдов для показа
+	slidesPerView: 3,
+	// Отступ между слайдами
+	spaceBetween: -10,
+	// Активный слайд по центру
+	centeredSlides: true,
+	// Бесконечный слайдер
+	loop: true,
+
+	// Автопрокрутка
+	autoplay: {
+		// Пауза между прокурткой
+		delay: 2000,
+		// Отключить после ручного переключения
+		disableOnInteraction: false,
+		// Оставновка при наведении мыши 
+		pauseOnMouseEnter: true,
+	},
+	// Скорость прокрутки
+	speed: 700,
+
+	// Куб
+	/*effect: 'cube',
+
+	// Дополнение к cube
+	cubeEffect: {
+
+	}*/
+	
+	// CoverFlow
+	effect: 'coverflow',
+
 });
