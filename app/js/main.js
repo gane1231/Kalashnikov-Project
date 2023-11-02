e3f04ba94a9196c58d8608b853874f6c56f2357d
Kirill-Konovalov <96065474+Kirill-Konovalov@users.noreply.github.com>
2023-11-02 11:48:52 +0400

    .

--- app/js/main.js
+++ app/js/main.js
@@ -18,3 +18,24 @@ const callback1 = () =>{
 btn.addEventListener('click', callback)
 apply.addEventListener('click', callback1)
 back.addEventListener('click', callback1)
+
+
+
+function onEntry(entry) {
+  entry.forEach(change => {
+    if (change.isIntersecting) {
+     change.target.classList.add('element-show');
+    }else{
+      change.target.classList.remove('element-show');
+    }
+  });
+}
+
+let options = {
+  threshold: [0] };
+let observer = new IntersectionObserver(onEntry, options);
+let elements = document.querySelectorAll('.element-animation');
+
+for (let elm of elements) {
+  observer.observe(elm);
+}
